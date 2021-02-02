25334575fe6ed2261e4c096e0203340b8e1d15e4
Ryan Thomas <rynthomas@yahoo.com>
2021-02-02 22:14:59 +0000

    added 'sportsipy' api to display sports data

--- games/views.py
+++ games/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from datetime import datetime
+from sportsipy.ncaab.schedule import Schedule
+from sportsipy.ncaab.boxscore import Boxscores
+
+
+def games(request):
+    """ View to return index page """
+
+    return render(request, 'games/games.html')
+
+
+games_today = Boxscores(datetime.today())
+print(games_today.games)
