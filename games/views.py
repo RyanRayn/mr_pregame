967857ac8c4a348c226807271747a6d93551875f
Ryan Thomas <rynthomas@yahoo.com>
2021-02-04 21:32:34 +0000

    uninstalled sportsipy api, added more styling to homepage

--- games/views.py
+++ games/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
 
-from datetime import datetime
-from sportsipy.ncaab.schedule import Schedule
-from sportsipy.ncaab.boxscore import Boxscores
-
 
 def games(request):
-    """ View to return index page """
+    """ View to return games page """
 
     return render(request, 'games/games.html')
-
-
-games_today = Boxscores(datetime.today())
-print(games_today.games)
