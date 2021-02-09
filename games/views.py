257e0d9b5068ccfba8eff780a532cc183c78d364
Ryan Thomas <rynthomas@yahoo.com>
2021-02-09 22:03:41 +0000

    worked on 'games' view to pas data into template

--- games/views.py
+++ games/views.py
@@ -1,24 +1,20 @@
 import requests
-
 from django.shortcuts import render
-from django.conf import settings
+
+from datetime import datetime
+from sportsipy.ncaab.boxscore import Boxscores
+from sportsipy.ncaab.schedule import Schedule
 
 
 def games(request):
     """ View to return games page """
 
-    date = '2021-FEB-06'
-    key = settings.SPORTSDATA_KEY
-    games_by_date = 'https://api.sportsdata.io/v3/cbb/scores/json/TeamGameStatsByDate/'
-
-    query_url = f"{games_by_date}{date}?key={key}"
-
-    r = requests.get(query_url)
-
-    results = r.json()
+    team_schedule = Schedule('PURDUE')
+    for game in team_schedule:
+        print(game.boxscore.away_total_rebounds)
 
     context = {
-        'results': results,
+        'team_schedule': team_schedule,
     }
 
     return render(request, 'games/games.html', context)
--- mr_pregame/settings.py
+++ mr_pregame/settings.py
@@ -155,5 +155,3 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-SPORTSDATA_KEY = os.getenv('SPORTSDATA_KEY', '')
