4cfc38b49f1de3c1ef59049e99514d3c0963844a
Ryan Thomas <rynthomas@yahoo.com>
2021-03-14 16:25:05 +0000

    restructured gamecards

--- games/views.py
+++ games/views.py
@@ -1,24 +1,36 @@
 import requests
 from django.shortcuts import render
 from django.conf import settings
-from datetime import datetime
+import datetime
 
 
 def games(request):
     """ View to return games page """
+    if request.method == "POST":
+        tomorrows_games = request.POST.get('date')
+       
+    params = {"league": "ncaab", "date": ""}
 
-    url = "https://sportspage-feeds.p.rapidapi.com/games?league=NCAAB"
+    url = "https://sportspage-feeds.p.rapidapi.com/games"
 
     headers = {
         'x-rapidapi-key': settings.SPORTSPAGE_FEEDS_KEY,
         'x-rapidapi-host': "sportspage-feeds.p.rapidapi.com"
         }
 
-    results = requests.request("GET", url, headers=headers).json()
+    results = requests.request("GET", url,
+                               headers=headers,
+                               params=params).json()
+
     games = results['results']
+    today = datetime.date.today()
+    tomorrow = today + datetime.timedelta(days=1)
+
     for game in games:
-        date = game['schedule']['date']
-        datetime_date = datetime.strptime(date, '%Y-%m-%dT%H:%M:%S.%fZ')
+        gamedate = game['schedule']['date']
+        datetime_date = datetime.datetime.strptime(
+                                                   gamedate,
+                                                   '%Y-%m-%dT%H:%M:%S.%fZ')
         game['game_date'] = datetime_date.strftime('%B %d, %Y')
         game['game_time'] = datetime_date.strftime('%-I:%M %p')
         if 'scoreboard' in game and 'score' in game['scoreboard']:
@@ -27,6 +39,8 @@ def games(request):
 
     context = {
         'games': games,
+        'today': today,
+        'tomorrow': tomorrow,
     }
 
     return render(request, 'games/games.html', context)
