a0706e33de32f29da57274c850ea955f908f31b9
Ryan Thomas <rynthomas@yahoo.com>
2021-03-17 20:59:09 +0000

    added functionality to switch leagues in navbar and added 'includes/basketball.html' to games templates

--- games/views.py
+++ games/views.py
@@ -6,11 +6,16 @@ import datetime
 
 def games(request):
     """ View to return games page """
+    todays_date = datetime.date.today()
+    today = todays_date.strftime('%Y-%m-%d')
+    tomorrow = todays_date + datetime.timedelta(days=1)
+
     if request.method == "GET":
         gameday = request.GET.get('gameDate')
+        league = request.GET.get('leagueName')
 
-    params = {"league": "nba", "date": gameday}
-    
+    params = {"league": league, "date": gameday}
+
     url = "https://sportspage-feeds.p.rapidapi.com/games"
 
     headers = {
@@ -23,10 +28,7 @@ def games(request):
                                params=params).json()
 
     games = results['results']
-    todays_date = datetime.date.today()
-    today = todays_date.strftime('%Y-%m-%d')
-    tomorrow = todays_date + datetime.timedelta(days=1)
-
+    print(games)
     for game in games:
         gamedate = game['schedule']['date']
         datetime_date = datetime.datetime.strptime(
@@ -34,17 +36,17 @@ def games(request):
                                                    '%Y-%m-%dT%H:%M:%S.%fZ')
         game['game_date'] = datetime_date.strftime('%B %d, %Y')
         game['game_time'] = datetime_date.strftime('%-I:%M %p')
-        
+
         if 'scoreboard' in game and 'score' in game['scoreboard']:
             score = game['scoreboard']['score']
             game['total'] = score['home'] + score['away']
-            
 
     context = {
         'games': games,
         'today': today,
         'tomorrow': tomorrow,
         'gameday': gameday,
+        'league': league,
     }
 
     return render(request, 'games/games.html', context)
