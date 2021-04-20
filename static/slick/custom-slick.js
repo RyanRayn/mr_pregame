000c6117ac4c3bd2cfc38de2812a18d3fe77ee47
Ryan Thomas <rynthomas@yahoo.com>
2021-04-20 20:58:00 +0000

    cleaned up the info-box on matchup page, started reorganizing the slick-slider for games display

--- static/slick/custom-slick.js
+++ static/slick/custom-slick.js
@@ -1,92 +1,11 @@
+/* Custom settings for carousel on matchup page */
 
 $(document).ready(function(){
     $('.slick-slider').slick({
-        infinite:true,
-        autoplay:true,
-        autoplaySpeed: 3000,
-        speed: 1000,
-        arrows:false,
-        slidesToShow: 12,
+        vertical: true,
+        slidesToShow: 1,
         slidesToScroll: 1,
-        draggable:true,
-        swipe:true,
-        swipeToSlide:true,
-        focusOnSelect:false,
-        variableWidth: true,
-        variableHeight: true,
-        responsive: [
-            {
-            breakpoint:1650,
-            settings: {
-                centerMode:true,
-                slidesToShow: 11,
-                slidesToScroll: 1,
-                infinite:true,
-                autoplay:true,
-                autoplaySpeed: 3000,
-                arrows:false,
-                draggable:true,
-                swipe:true,
-                swipeToSlide:true,
-                focusOnSelect:false,
-                variableWidth: true,
-                variableHeight: true
-            }
-            },
-            {
-            breakpoint:1400,
-            settings: {
-                centerMode:true,
-                slidesToShow: 9,
-                slidesToScroll: 1,
-                infinite:true,
-                autoplay:true,
-                autoplaySpeed: 3000,
-                arrows:false,
-                draggable:true,
-                swipe:true,
-                swipeToSlide:true,
-                focusOnSelect:false,
-                variableWidth: true,
-                variableHeight: true
-            }    
-            },
-            {
-            breakpoint: 1120,
-            settings: {
-                centerMode:true,
-                slidesToShow: 8,
-                slidesToScroll: 1,
-                infinite:true,
-                autoplay:true,
-                autoplaySpeed: 3000,
-                arrows:false,
-                draggable:true,
-                swipe:true,
-                swipeToSlide:true,
-                focusOnSelect:false,
-                variableWidth: true,
-                variableHeight: true
-            }
-            },
-            {
-            breakpoint: 600,
-            settings: {
-                centerMode:true,
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite:true,
-                autoplay:true,
-                autoplaySpeed: 3000,
-                arrows:false,
-                draggable:true,
-                swipe:true,
-                swipeToSlide:true,
-                focusOnSelect:false,
-                variableWidth: true,
-                variableHeight: true
-            }
-            },
-        ]
+        autoplay: true,
+        arrows: true,
     });
 });
