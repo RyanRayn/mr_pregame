82392a21b2a083a15ad4485eda22448e4f81c28a
Ryan Thomas <rynthomas@yahoo.com>
2021-05-25 07:22:35 +0000

    changed order of installed apps

--- mr_pregame/settings.py
+++ mr_pregame/settings.py
@@ -36,16 +36,16 @@ ALLOWED_HOSTS = ['mrpregame.herokuapp.com', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',
     'django.contrib.humanize',
-    'allauth',
-    'allauth.account',
     'allauth.socialaccount',
     'crispy_forms',
     'django_countries',
